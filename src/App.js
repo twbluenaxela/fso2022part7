507c7bd0f5d9b503943c75903822a5624aeee0f9
Nick Yu <rednaxela64@outlook.com>
2022-09-08 11:17:26 -0700

    implement prototype of page swtiching

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import './App.css';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
 
-function App() {
+const Home = () => (
+  <div>
+    <h2>TKTL notes app</h2>
+  </div>
+)
+
+const Notes = () => (
+  <div>
+    <h2>Notes</h2>
+  </div>
+)
+
+const Users = () => (
+  <div>
+    <h2>Users</h2>
+  </div>
+)
+
+const App = () => {
+
+  const [page, setPage] = useState('home')
+  
+  const toPage = (page) => (event) => {
+    event.preventDefault()
+    setPage(page)
+  }
+
+  const content = () => {
+    switch(page){
+      case 'home':
+        return <Home />
+      case 'notes':
+        return <Notes />
+      case 'users':
+        return <Users />
+      default: 
+        return <Home />
+    }
+  }
+
+  const padding = {
+    padding: 5
+  }
+
   return (
-    <div className="App">
+    <div>
+      <div>
+        <a href="" onClick={toPage('home')} style={padding} >
+          home
+        </a>
+        <a href="" onClick={toPage('notes')} style={padding} >
+          notes
+        </a>
+        <a href="" onClick={toPage('users')} style={padding} >
+          users
+        </a>
+      </div>
+      {content()}
     </div>
   );
 }
