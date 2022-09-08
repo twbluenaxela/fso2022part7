b2a66ea9fe7861bb3e7a5dfc05dfd6ba19343c58
Nick Yu <rednaxela64@outlook.com>
2022-09-08 12:36:12 -0700

    move router element to be rendered at very top level

--- src/App.js
+++ src/App.js
@@ -1,66 +1,158 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom/client'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useParams,
+  useNavigate,
+  useMatch,
+} from "react-router-dom";
 
 const Home = () => (
   <div>
     <h2>TKTL notes app</h2>
+    <p>Lorem ipsum text.....</p>
   </div>
-)
+);
 
-const Notes = () => (
+const Note = ({ note }) => {
+  // const id = useParams().id;
+  // const note = notes.find((n) => n.id === Number(id));
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>{note.important ? "important" : ""}</strong>
+      </div>
+    </div>
+  );
+};
+
+const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
-)
+);
 
 const Users = () => (
   <div>
     <h2>Users</h2>
+    <ul>
+      <li>Matti Luukainen</li>
+      <li>Juha Taurianen</li>
+      <li>Arto Hellas</li>
+    </ul>
   </div>
-)
+);
 
-const App = () => {
+const Login = (props) => {
+  const navigate = useNavigate();
 
-  const [page, setPage] = useState('home')
-  
-  const toPage = (page) => (event) => {
-    event.preventDefault()
-    setPage(page)
-  }
-
-  const content = () => {
-    switch(page){
-      case 'home':
-        return <Home />
-      case 'notes':
-        return <Notes />
-      case 'users':
-        return <Users />
-      default: 
-        return <Home />
-    }
-  }
-
-  const padding = {
-    padding: 5
-  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    props.onLogin("mluukai");
+    navigate("/");
+  };
 
   return (
     <div>
-      <div>
-        <a href="" onClick={toPage('home')} style={padding} >
-          home
-        </a>
-        <a href="" onClick={toPage('notes')} style={padding} >
-          notes
-        </a>
-        <a href="" onClick={toPage('users')} style={padding} >
-          users
-        </a>
-      </div>
-      {content()}
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type="password" />
+        </div>
+        <button type="submit">login</button>
+      </form>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false,
+      user: "Matti Luukkainen",
+    },
+    {
+      id: 3,
+      content: "Most important methods of HTTP-protocol are GET and POST",
+      important: true,
+      user: "Arto Hellas",
+    },
+  ]);
+
+  const [user, setUser] = useState(null);
+  const match = useMatch('/notes/:id')
+  const note = match 
+  ? notes.find(note => note.id === Number(match.params.id))
+  : null
+
+  const login = (user) => {
+    setUser(user);
+  };
+
+  const padding = {
+    padding: 5,
+  };
+
+  return (
+    <div>
+        <div>
+          <Link style={padding} to="/">
+            home
+          </Link>
+          <Link style={padding} to="/notes">
+            notes
+          </Link>
+          <Link style={padding} to="/users">
+            users
+          </Link>
+          {user ? (
+            <em>{user} logged in</em>
+          ) : (
+            <Link style={padding} to="/login">
+              login
+            </Link>
+          )}
+        </div>
+        <Routes>
+          <Route path="/notes/:id" element={<Note note={note} />} />
+          <Route path="/notes" element={<Notes notes={notes} />} />
+          <Route
+            path="/users"
+            element={user ? <Users /> : <Navigate replace to="/login" />}
+          />
+          <Route path="/login" element={<Login onLogin={login} />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      <div>
+        <br />
+        <em>Note app, Department of Computer Science 2022</em>
+      </div>
+    </div>
+  );
+};
 
 export default App;
